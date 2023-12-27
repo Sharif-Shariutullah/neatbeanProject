f48067f253fb13076b9fca1f9e3164c067dd4eb3
Shariful Islam <151622805+Sharif-Shariutullah@users.noreply.github.com>
2023-12-27 17:29:47 +0600

    Webtechnology

---
+++ WebApplication1/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Dec 27, 2023, 5:25:48 PM
+    Author     : B-5
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
--- first_Second_large/src/first_second_large/First_Second_large.java
+++ first_Second_large/src/first_second_large/First_Second_large.java
@@ -1,14 +1,26 @@
-
 package first_second_large;
 
+import java.lang.invoke.LambdaMetafactory;
+import java.util.Scanner;
 
 public class First_Second_large {
 
-    
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-       
-        System.out.println(asldkfja);
-        
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+        if (N % 2 == 0) {
+            System.out.println("Weird");
+        } else if(N % 2 ==0  && 2>=5) {
+            System.out.println(" Not Weird ");
+        } else if(N % 2 ==0  && 6>=20) {
+            System.out.println(" Weird ");
+        } 
+        else {
+            System.out.println("Not Weird");
+        }
+
     }
-    
 }
